b433563763fe3f0b09d181f518e9304e540dd256
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-22 20:20:10 +0330

    author table created

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+PROFESSIONS = (
+    ("نویسنده", "AUTHOR"),
+    ("ویراستار", "EDITOR"),
+    ("نویسنده", "TRANSLATOR"),
+)
+
+
+class Authors(models.Model):
+    name = models.CharField(max_length=50, verbose_name="نام")
+    profession = models.CharField(
+        choices=PROFESSIONS, max_length=3, verbose_name="حرفه"
+    )
+    heigh = models.CharField(max_length=10, verbose_name="قد")
+    birth_date = models.CharField(max_length=50, verbose_name="تاریخ تولد")
+    birth_place = models.CharField(max_length=50, verbose_name="محل تولد")

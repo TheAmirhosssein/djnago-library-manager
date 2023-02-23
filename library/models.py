3e9a96d278197379c5cd9029af5d6a8ae21c96ac
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-23 22:18:41 +0330

    books table created

--- library/models.py
+++ library/models.py
@@ -49,3 +49,29 @@ class Publishers(models.Model):
 
     def __str__(self) -> str:
         return super().__str__(self.title)
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=50, verbose_name="عنوان")
+    slug = models.SlugField(
+        db_index=True, null=False, default="", unique=True, editable=True
+    )
+    translator = models.ForeignKey(
+        Translators, verbose_name="مترجم", on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        Authors, verbose_name="نویسنده", on_delete=models.CASCADE
+    )
+    genres = models.ManyToManyField(Genres, verbose_name="ژانر")
+    publisher = models.ForeignKey(
+        Publishers, verbose_name="نویسنده", on_delete=models.CASCADE
+    )
+    book_image = models.ImageField(upload_to="images/books")
+    detail = models.TextField(verbose_name="توضیحات")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return super().__str__(self.title)

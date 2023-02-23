a9b04d5d244bf795ed2e749a1fb2f73be80f5015
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-23 15:20:58 +0330

    translators model created

--- library/models.py
+++ library/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
-PROFESSIONS = (
-    ("نویسنده", "AUTHOR"),
-    ("ویراستار", "EDITOR"),
-    ("منرجم", "TRANSLATOR"),
-)
-
 
 class Authors(models.Model):
     name = models.CharField(max_length=50, verbose_name="نام")
-    profession = models.CharField(
-        choices=PROFESSIONS, max_length=3, verbose_name="حرفه"
-    )
+    slug = models.SlugField(db_index=True, null=False, default="", unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return super().__str__(self.name)
+
+
+class Translators(models.Model):
+    name = models.CharField(max_length=50, verbose_name="نام")
     slug = models.SlugField(db_index=True, null=False, default="", unique=True)
 
     def save(self, *args, **kwargs):

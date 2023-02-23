cc73fe783366c99bff876df6e588c02d2092fdbd
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-23 15:14:10 +0330

    created publishers model

--- library/models.py
+++ library/models.py
@@ -33,3 +33,16 @@ class Genres(models.Model):
 
     def __str__(self) -> str:
         return super().__str__(self.title)
+
+
+class Publishers(models.Model):
+    title = models.CharField(max_length=50, verbose_name="عنوان")
+    website = models.URLField(verbose_name="وب سایت")
+    slug = models.SlugField(db_index=True, null=False, default="", unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return super().__str__(self.title)

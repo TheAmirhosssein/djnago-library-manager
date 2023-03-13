3f0a091d435f229dee3c0ab24b86208d6ea0130c
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-03-13 16:57:02 +0330

    book detail url edited

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,6 @@ from library.views import AuthorView, BookDetailView, HomeView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("book/<slug>/", BookDetailView.as_view(), name="details"),
+    path("book/<slug>/", BookDetailView.as_view(), name="book_details"),
     path("author/<slug>/", AuthorView.as_view(), name="author"),
 ]

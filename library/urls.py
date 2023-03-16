0743a92bf05b4876ae6b2f460692dce68b7ed548
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-03-16 16:48:51 +0330

    genres urls added

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from library.views import AuthorView, BookDetailView, HomeView, TranslatorView
+from library.views import (
+    AuthorView,
+    BookDetailView,
+    GenresView,
+    HomeView,
+    TranslatorView,
+)
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("book/<slug>/", BookDetailView.as_view(), name="book_details"),
     path("author/<slug>/", AuthorView.as_view(), name="author"),
     path("translator/<slug>/", TranslatorView.as_view(), name="translator"),
+    path("genres/<slug>/", GenresView.as_view(), name="genres"),
 ]

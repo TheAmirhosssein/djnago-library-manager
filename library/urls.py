7f23c4ec79ec33020897ed269db3d5254cb42be1
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-03-16 16:38:04 +0330

    translator url edited

--- library/urls.py
+++ library/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("book/<slug>/", BookDetailView.as_view(), name="book_details"),
     path("author/<slug>/", AuthorView.as_view(), name="author"),
-    path("translator/<slug>/", TranslatorView.as_view(), name="author"),
+    path("translator/<slug>/", TranslatorView.as_view(), name="translator"),
 ]

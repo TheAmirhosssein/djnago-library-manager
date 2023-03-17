3fa3a80d75e5f2e3ddcb64b223d8806283a2bdb6
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-03-17 17:33:11 +0330

    main url edited

--- conf/urls.py
+++ conf/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", "library.urls"),
+    path("", include("library.urls")),
 ]

4b9264480828a02ffd5bebb7b481e06c4a23962c
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-03-18 18:03:53 +0330

    models registerd in admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from library import models
+
+admin.site.register(models.Authors)
+admin.site.register(models.Books)
+admin.site.register(models.Genres)
+admin.site.register(models.Publishers)
+admin.site.register(models.Translators)

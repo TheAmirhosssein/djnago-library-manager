69871e2486f9b04ec06763245a109c3a1f5020e3
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-03-19 16:51:03 +0330

    models edited

--- library/models.py
+++ library/models.py
@@ -4,57 +4,62 @@ from django.template.defaultfilters import slugify
 
 class Authors(models.Model):
     name = models.CharField(max_length=50, verbose_name="نام")
-    slug = models.SlugField(db_index=True, null=False, default="", unique=True)
+    slug = models.SlugField(
+        db_index=True, null=False, default="", unique=True, editable=False
+    )
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
-    def __str__(self) -> str:
-        return super().__str__(self.name)
+    def __str__(self):
+        return self.name
 
 
 class Translators(models.Model):
     name = models.CharField(max_length=50, verbose_name="نام")
-    slug = models.SlugField(db_index=True, null=False, default="", unique=True)
+    slug = models.SlugField(
+        db_index=True, null=False, default="", unique=True, editable=False
+    )
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
-    def __str__(self) -> str:
-        return super().__str__(self.name)
+    def __str__(self):
+        return self.name
 
 
 class Genres(models.Model):
     title = models.CharField(max_length=50, verbose_name="عنوان")
-    slug = models.SlugField(db_index=True, null=False, default="", unique=True)
+    slug = models.SlugField(
+        db_index=True, null=False, default="", unique=True, editable=False
+    )
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
-    def __str__(self) -> str:
-        return super().__str__(self.title)
+    def __str__(self):
+        return self.title
 
 
 class Publishers(models.Model):
     title = models.CharField(max_length=50, verbose_name="عنوان")
     website = models.URLField(verbose_name="وب سایت")
-    slug = models.SlugField(db_index=True, null=False, default="", unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
-    def __str__(self) -> str:
-        return super().__str__(self.title)
+    def __str__(self):
+        return self.title
 
 
 class Books(models.Model):
     title = models.CharField(max_length=50, verbose_name="عنوان")
     slug = models.SlugField(
-        db_index=True, null=False, default="", unique=True, editable=True
+        db_index=True, null=False, default="", unique=True, editable=False
     )
     translator = models.ForeignKey(
         Translators, verbose_name="مترجم", on_delete=models.CASCADE
@@ -64,7 +69,7 @@ class Books(models.Model):
     )
     genres = models.ManyToManyField(Genres, verbose_name="ژانر")
     publisher = models.ForeignKey(
-        Publishers, verbose_name="نویسنده", on_delete=models.CASCADE
+        Publishers, verbose_name="ناشر", on_delete=models.CASCADE
     )
     book_image = models.ImageField(upload_to="images/books")
     detail = models.TextField(verbose_name="توضیحات")
@@ -74,4 +79,4 @@ class Books(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self) -> str:
-        return super().__str__(self.title)
+        return f"{self.title} | {self.author}"

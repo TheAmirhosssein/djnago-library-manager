e9ce02cd84aa80cf690b1fcdd7c7802f6940c115
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-04-05 23:49:36 +0330

    author filter added to booklist admin page

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("books/<pk>/update/", views.UpdateBookView.as_view(), name="book_update"),
     path("books/<pk>/delete/", views.DeleteBookView.as_view(), name="book_delete"),
     path("books/search/", views.SearchBookView.as_view(), name="book_search"),
+    path("books/filter/<author>", views.BooksListView.as_view(), name="book_author"),
     # authors
     path("authors/", views.AuthorsListView.as_view(), name="authors_admin"),
     path("authors/create/", views.AddAuthorsView.as_view(), name="authors_create"),

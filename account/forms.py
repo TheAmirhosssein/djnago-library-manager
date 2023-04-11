fc30eef3bd6b8961fe2b0b2424ad0553c67456b8
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-04-11 21:48:11 +0330

    register edited

--- account/forms.py
+++ account/forms.py
@@ -1,21 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import (
-    PasswordChangeForm,
-    SetPasswordForm,
-    UserCreationForm,
-)
+from django.contrib.auth.forms import (PasswordChangeForm, SetPasswordForm,
+                                       UserCreationForm)
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 
 # registration user form
-class RegisterForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=60, required=True, help_text="Fast Name")
     last_name = forms.CharField(max_length=60, required=True, help_text="Last Name")
     username = forms.CharField(max_length=150, required=True, help_text="Username")
     email = forms.EmailField(max_length=254, required=True, help_text="Email")
-    password2 = forms.CharField(widget=forms.PasswordInput(), required=True)
-    password = forms.CharField(widget=forms.PasswordInput(), required=True)
 
     class Meta:
         model = User
@@ -27,10 +22,3 @@ class RegisterForm(forms.ModelForm):
             "password",
         ]
 
-    def clean_confirm_password(self):
-        password = self.cleaned_data.get("password")
-        confirm_password = self.cleaned_data.get("password2")
-        if password == confirm_password:
-            return confirm_password
-
-        raise ValidationError("کلمه عبور و تکرار کلمه عبور مغایرت دارند")
--- account/views.py
+++ account/views.py
@@ -1,10 +1,30 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from account.forms import RegisterForm
 
 
 class RegisterView(CreateView):
-    template_name = "account/register.html"
+    model = User
     form_class = RegisterForm
-    success_url = "/account/register/"
+    success_url = reverse_lazy("register")
+    template_name = "account/register.html"
+
+
+
+# class CreateUser(SetNotAccessMixin, ReturnIfAuthenticateMixin, CreateView):
+#     form_class = UserForm
+#     model = User
+
+#     def form_valid(self, form):
+#         user = form.save(commit=False)
+#         user.is_active = False
+#         user.save()
+#         return render(self.request, "email-confirmation/email_send_login.html")
+
+#     # if is success rever user to login page
+#     success_url = reverse_lazy("login")
+#     # our template name
+#     template_name = "registration/signup.html"

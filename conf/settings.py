56b9974ff78608e3a6c29b836036bd69cf0756bc
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-04-11 21:30:21 +0330

    global static setting added

--- conf/settings.py
+++ conf/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "widget_tweaks",
     "library",
     "admin_panel",
+    "account",
 ]
 
 MIDDLEWARE = [
@@ -108,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

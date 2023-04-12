6898d05e24cca939c682397562240c3796c255ca
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-04-12 22:55:03 +0330

    login view added

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ from account import views
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]

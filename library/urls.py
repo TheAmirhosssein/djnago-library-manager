d08c7dca8474b50937724f810859f922d4321e1b
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-06-01 20:52:54 +0330

    urls edited

--- library/urls.py
+++ library/urls.py
@@ -5,7 +5,7 @@ from library.views import (AuthorView, BookDetailView, GenresView, HomeView,
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("book/<author>/", HomeView.as_view(), name="author_filter"),
+    path("books_filter/<author>/", HomeView.as_view(), name="author_filter"),
     path("book/<slug>/", BookDetailView.as_view(), name="book_details"),
     path("author/<slug>/", AuthorView.as_view(), name="author"),
     path("translator/<slug>/", TranslatorView.as_view(), name="translator"),
